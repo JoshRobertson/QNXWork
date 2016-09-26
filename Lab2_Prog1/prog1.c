71e91806e095c2116b8a10ff1b770a3a41712b80
Josh Robertson <robe0591@algonquinlive.com>
2016-09-26 12:43:06 -0400

    finish prog 1

--- Lab2_Prog1/prog1.c
+++ Lab2_Prog1/prog1.c
@@ -4,43 +4,50 @@
  * Mod. by:  Carolina Ayala
  * compile with -std=gnu99 flag!
  */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
 
-void sigint_handler(int sig);
 
+void sig_handler(int sig);
+sig_atomic_t usr1Happened = 0;
 
 /*******************************************************************************
  * main( )
  ******************************************************************************/
 int main(void) {
-
 	char s[140];
 	struct sigaction sa;
+	memset(&sa, 0, sizeof(sa));
 
-	sa.sa_handler = sigint_handler;
+	sa.sa_handler = sig_handler;
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
+
+	printf("PID = %d : Running...\n", getpid());
 
 	if (sigaction(SIGINT, &sa, NULL) == -1) {
 		perror("sigaction");
 		exit(1);
 	}
+    sigaction(SIGUSR1, &sa, NULL);
 
-	printf("Enter a string:  ");
+    while(usr1Happened != 1){} //Loop until SIGUSR1
 
-	if (fgets(s, sizeof s, stdin) == NULL)
-		perror("fgets");
-	else
-		printf("You entered: %s\n", s);
+    printf("usr1Happened: %d\n", usr1Happened);
+    printf("PID = %d : Exiting...\n", getpid());
 
 	return 0;
 }
 
-void sigint_handler(int sig) {
-	write(0, "\nAhhh! SIGINT!\n", 14);
+void sig_handler(int sig){
+	if (sig == SIGUSR1){
+    	usr1Happened = 1;
+    	printf("PID = %d : Received USR1\n", getpid());
+	}
+	else
+		write(0, "\nAhhh! SIGINT!\n", 14);
 }
